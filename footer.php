e117c6da1baa70c24ad95449b869096e55a0685b
misterair <mister.air@gmail.com>
2014-02-03 17:24:52 +0100

    fix toolbar

--- footer.php
+++ footer.php
@@ -3,5 +3,6 @@
 			<a class="linkRSS" href="<?php bloginfo('rss2_url'); ?>">RSS</a> "<a href="https://github.com/misterair/FlatAndQuiet">Flat and Quiet</a>", un thème propulsé par Wordpress.
 		</div>
 	</footer>
+	<?php wp_footer(); ?>
 </body>
 </html>
